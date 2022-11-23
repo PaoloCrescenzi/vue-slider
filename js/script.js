9d0a97d20be65528f6dd66c37744f47ce7ac4f54
PaoloCrescenz <crescenzipaolo2@gmail.com>
2022-11-23 09:51:51 +0100

    immagini

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const { createApp } = Vue;
+
+createApp({
+    // data: function () {}
+    data() {
+        // la funzione data deve SEMPRE ritornare un oggetto
+        return {
+            currentImageIndex: 0,
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
+        };   
+    },
+    methods: {
+        backImg() {
+            if (this.currentImageIndex === 0) {
+                this.currentImageIndex = this.slides.length - 1;
+
+            } else {
+                this.currentImageIndex --;
+            }
+        },
+
+        nextImg() {
+            if (this.currentImageIndex === this.slides.length - 1) {
+                this.currentImageIndex = 0;
+            } else {
+                this.currentImageIndex ++;
+            }
+        },
+
+        onAnteprimaClick(ImmagineCliccata){
+            this.currentImageIndex = ImmagineCliccata;
+        }
+
+
+
+    }
+}).mount("#app")
+
+
+
